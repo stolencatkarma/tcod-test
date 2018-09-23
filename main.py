a635134f299315f9d39e6668cee848c1c0aca3a3
stolencatkarma <enjia2000@gmail.com>
2018-09-23 12:30:08 -0400

    cumulative

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import tcod.map
 import os
 import json
 from collections import defaultdict
+import math
 
 class Tile:
     # a tile of the map and its properties
@@ -34,8 +35,8 @@ class Object:
  
     def move(self, x, y):
         # move to the given position
-        self.x = x
-        self.y = y
+        self.x = self.x + x
+        self.y = self.y + y
  
     def draw(self, console):
         # set the color and then draw the character that represents this object at its position
@@ -46,6 +47,20 @@ class Object:
         # erase the character that represents this object
         tcod.console_put_char(console, self.x, self.y, ' ', tcod.BKGND_NONE)
 
+class Door(Object):
+    def __init__(self, x, y, char, color):
+        super().__init__(x, y, char, color)
+        self.impassable = True
+        self.block_sight = True
+
+    def on_examined(self, x, y, objects):
+        if(self.impassable):
+            self.impassable = None
+            self.block_sight = None
+        else:
+            self.impassable = True
+            self.block_sight = True
+
 class Player(Object):
     def __init__(self, x, y, char, color, name):
         super().__init__(x, y, char, color)
@@ -53,6 +68,18 @@ class Player(Object):
         self.stats = dict()
         self.hp = 10
     
+    def examine(self, x, y, objects):
+        global current_level
+        # look for an object with an on_examined() at the x,y
+        for obj in objects:
+            if obj.x == x and obj.y == y:
+                _result = obj.on_examined(x,y,objects)
+                if(_result == 'up'):
+                    current_level = current_level - 1
+                elif(_result == 'down'):
+                    current_level = current_level + 1
+                else:
+                    pass
 
 class Monster(Object):
     def __init__(self, x, y, char, color, reference):
@@ -69,6 +96,15 @@ class Stairs(Object):
     def __init__(self, x, y, char, color):
         # char decides direction. < is down. > is up. player just needs to activate stairs. not choose direction.
         super().__init__(x, y, char, color)
+    
+    def on_examined(self, x, y, objects):
+       # move the player down or up a level depending on the stairs.
+        if(self.char == tcod.CHAR_ARROW2_N):
+           # up stairs
+           return 'up'
+        else:
+            # down stairs
+            return 'down'
         
 class MonsterManager:
     def __init__(self, root_console):
@@ -132,7 +168,7 @@ def make_map(difficulty): # how deep or difficult should the level be we generat
 
     if(len(rooms) == 0): # this is the first room on this level. put up stairs here.
         # put UP Stairs where the player went down from.
-        objects.append(Stairs(player.x, player.y, tcod.CHAR_ARROW2_N, tcod.color.Color(25, 25, 25)))
+        objects.append(Stairs(player.x, player.y, tcod.CHAR_ARROW2_N, tcod.color.Color(25, 125, 125)))
         # draw a simple room around the player.
         for y in range(player.y - 1, player.y + 2):
             for x in range(player.x - 1, player.x + 2):
@@ -141,29 +177,111 @@ def make_map(difficulty): # how deep or difficult should the level be we generat
     
     cursor_y = player.y # use this is a basepoint to draw the next rooms.
     cursor_x = player.x
-    room_distance = random.randint(4,5)
-    
-    
+        
     should_continue = True
     while should_continue:
         if(next_room == 'blank'):
             # need to make a decent sized room then loop and make another room until we run out of room for rooms. =)
-            next_room_width = random.randint(2,6)
-            next_room_height = random.randint(2,4)
+            next_room_width = random.randint(3,7)
+            next_room_height = random.randint(3,7)
+            room_distance = random.randint(6,8)
             # find a spot for the next room and then connect the two rooms with a hallway.
             
+            if(cursor_y <= 1):
+                cursor_y = 2
+            if(cursor_y >= MAP_HEIGHT):
+                cursor_y = MAP_HEIGHT-2
+            
+            if(cursor_x <= 1):
+                cursor_x = 2
+            if(cursor_x >= MAP_WIDTH):
+                cursor_x = MAP_WIDTH-2
+                       
+
             can_build_up = True
-            for j in range(cursor_y-room_distance, cursor_y-room_distance + next_room_height):
+            for j in range(cursor_y - room_distance, cursor_y - room_distance + next_room_height):
                 for i in range(cursor_x, cursor_x + next_room_width):
-                    if(_check_map[j][i]):
+                    try:
+                        if(_check_map[j][i]):
+                            can_build_up = False
+                    except KeyError:
                         can_build_up = False
-                        
             if(can_build_up):
-                for j in range(cursor_y-room_distance, cursor_y-room_distance + next_room_height):
+                for j in range(cursor_y - room_distance, cursor_y - room_distance + next_room_height):
                     for i in range(cursor_x, cursor_x + next_room_width):
                         new_map[j][i].char = ' '
-                        cursor_y = cursor_y - room_distance
+                        _check_map[j][i] = True
+                for k in range(room_distance):
+                    new_map[cursor_y - k][cursor_x].char = ' '
+                    if(k is room_distance // 2):
+                        objects.append(Door(cursor_x, cursor_y-k, 'U', tcod.blue))
+                    _check_map[cursor_y - k][cursor_x] = True
+               
+                continue
             
+            can_build_left = True
+            for j in range(cursor_y, cursor_y + next_room_height):
+                for i in range(cursor_x - room_distance, cursor_x - room_distance + next_room_width):
+                    try:
+                        if(_check_map[j][i]):
+                            can_build_left = False
+                    except KeyError:
+                        can_build_left = False
+            if(can_build_left):
+                for j in range(cursor_y, cursor_y + next_room_height):
+                    for i in range(cursor_x - room_distance, cursor_x - room_distance + next_room_width):
+                        new_map[j][i].char = ' '
+                        _check_map[j][i] = True
+                for k in range(room_distance):
+                    new_map[cursor_y][cursor_x-k].char = ' '
+                    _check_map[cursor_y][cursor_x-k] = True
+                
+                continue
+
+            can_build_down = True
+            for j in range(cursor_y + room_distance, cursor_y + room_distance + next_room_height):
+                for i in range(cursor_x, cursor_x + next_room_width):
+                    try:
+                        if(_check_map[j][i]):
+                            can_build_down = False
+                    except KeyError:
+                        can_build_down = False
+            if(can_build_down):
+                for j in range(cursor_y+room_distance, cursor_y + room_distance + next_room_height):
+                    for i in range(cursor_x, cursor_x + next_room_width):
+                        new_map[j][i].char = ' '
+                        _check_map[j][i] = True
+                for k in range(room_distance):
+                    new_map[cursor_y + k][cursor_x].char = ' '
+                    _check_map[cursor_y + k][cursor_x] = True
+                
+                continue
+            
+            can_build_right = True
+            for j in range(cursor_y, cursor_y + next_room_height):
+                for i in range(cursor_x + room_distance, cursor_x + room_distance + next_room_width):
+                    try:
+                        if(_check_map[j][i]):
+                            can_build_right = False
+                    except KeyError:
+                        can_build_right = False
+            if(can_build_right):
+                for j in range(cursor_y, cursor_y + next_room_height):
+                    for i in range(cursor_x + room_distance, cursor_x + room_distance + next_room_width):
+                        new_map[j][i].char = ' '
+                        _check_map[j][i] = True
+                for k in range(room_distance):
+                    new_map[cursor_y][cursor_x+k].char = ' '
+                    _check_map[cursor_y][cursor_x+k] = True
+                
+                continue
+            
+            
+
+
+            # if we made it here without finding a place to build
+
+            objects.append(Stairs(cursor_x, cursor_y, tcod.CHAR_ARROW2_S, tcod.color.Color(125, 125, 25)))
             should_continue = False
                 
 
@@ -216,11 +334,10 @@ class dungeon_level(): # each dungeon level should have it's own references to t
 
 
 player = Player(WIDTH // 3, HEIGHT // 2, '@', tcod.white, 'The Hero')
-current_level = 0 # level zero is a special starting area.
 full_dungeon_stack = dict() # i want to save all previously generated levels so the player can go back to them. dict so we can have the freedom to make level 8 before 5,6,7 if we need to.
-full_dungeon_stack[0] = dungeon_level(1) # full_dungeon_stack[0].map would be town.
+full_dungeon_stack[0] = dungeon_level(0) # full_dungeon_stack[0].map would be town.
 full_dungeon_stack[0].creatures.append(player)
-    
+current_level = 0 # level zero is a special starting area.
 
 
 
@@ -243,10 +360,16 @@ with tcod.console_init_root(WIDTH, HEIGHT, 'tcod-test', False) as root_console:
         # if state is 'main' blit the map and the player on the screen as well as the noise level.
         elif(state == 'main'):
             # figure out what level we are on and use that data to display.
-            _level = full_dungeon_stack[current_level]
+            try:
+                _level = full_dungeon_stack[current_level]
+            except KeyError:
+                full_dungeon_stack[current_level] = dungeon_level(current_level)
+                _level = full_dungeon_stack[current_level]
             _map = _level.map
             _creatures = _level.creatures
             _objects = _level.objects
+
+            player.clear(root_console)
             for y in range(MAP_HEIGHT):
                 for x in range(MAP_WIDTH):
                     tcod.console_put_char(root_console, x, y, _map[y][x].char)
@@ -262,13 +385,9 @@ with tcod.console_init_root(WIDTH, HEIGHT, 'tcod-test', False) as root_console:
                     creature.do_action() # have creatures do thier move or attack here.
                     creature.draw(root_console) # finally redraw
             
-            player.clear(root_console)
             player.draw(root_console)
-            
-                
-
             # TODO: blit weather or fog effects.
-            
+            tcod.console_flush()
             # wait for input - parse input and do the following.
             # if key is up, down, left, right - move player or attack
             # if key is 
@@ -276,28 +395,55 @@ with tcod.console_init_root(WIDTH, HEIGHT, 'tcod-test', False) as root_console:
             # if key F1 open help screen
 
             ev = tcod.sys_wait_for_event(tcod.EVENT_KEY_RELEASE, key, None, True)
-            if ev :
-                root_console.print_(0, 0, repr(key))
-                _x = random.randint(0,MAP_WIDTH-1)
+            if ev:
+                # root_console.print_(0, 0, repr(key))
+                '''_x = random.randint(0,MAP_WIDTH-1)
                 _y = random.randint(0,MAP_HEIGHT-1)
-                _creatures.append(Monster(_x, _y, MM.MONSTER_TYPES['zombie']['char'],MM.MONSTER_TYPES['zombie']['color'], MM.MONSTER_TYPES['zombie']))
+                _creatures.append(Monster(_x, _y, MM.MONSTER_TYPES['zombie']['char'],MM.MONSTER_TYPES['zombie']['color'], MM.MONSTER_TYPES['zombie']))'''
+                if key.vk == tcod.KEY_CHAR:
+                    #TODO handle player attacks
+                    if key.c == ord('w'):
+                        player.move(0, -1)
+                    elif key.c == ord('s'):
+                        player.move(0, 1)
+                    elif key.c == ord('a'):
+                        player.move(-1, 0)
+                    elif key.c == ord('d'):
+                        player.move(1, 0)
+                    elif key.c == ord('e'): # (e)xamine or use
+                        root_console.print_(MAP_HEIGHT//2, MAP_WIDTH//2, 'Examine what? valid keys. e, w, s, a, d')
+                        tcod.console_flush() # flush to draw the examine box.
+                        ev = tcod.sys_wait_for_event(tcod.EVENT_KEY_RELEASE, key, None, True)
+                        if ev:
+                            if key.c == ord('e'): # (e)xamine here.
+                                player.examine(player.x, player.y, _objects)
+                            elif key.c == ord('w'):
+                                player.examine(player.x, player.y-1, _objects)
+                            elif key.c == ord('s'):
+                                player.examine(player.x, player.y+1, _objects)
+                            elif key.c == ord('a'):
+                                player.examine(player.x-1, player.y, _objects)
+                            elif key.c == ord('d'):
+                                player.examine(player.x+1, player.y, _objects)
+
+                        
+                    elif key.c == ord('1'):
+                        # use attack skill 1
+                        pass
+                    elif key.c == ord('2'):
+                        # use attack skill 2
+                        pass
+                    elif key.c == ord('3'):
+                        # use chosen skill 1
+                        pass
+                    elif key.c == ord('4'):
+                        # use chosen skill 2
+                        pass
+
             
-            
+                   
 
         
-        # if the player is grabbing items from a container.
-        elif(state == 'grab_items'):
-            # blit player's inventory space left and grabbable items
-            # assign a letter to all grabable items
-            # wait for input - if a letter assigned to an item grab the item.
-            pass
-
-        # if state is 'inventory' show items of value full screen.
-        elif(state == 'inventory'):
-            # assign letters to each item.
-            # if key pressed then drop the item destroying it.
-            pass
-
         # if state is 'help' show help screen.
         elif(state == 'help'):
             # blit all the commands in a help window.
